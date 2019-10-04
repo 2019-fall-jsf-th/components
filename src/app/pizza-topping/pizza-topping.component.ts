d30d15a0096017bd13ed1f2c9a239deeed211d4b
gitrylyn <ellexiong@gmail.com>
2019-10-03 20:13:48 -0500

    Got total calculations working

--- src/app/pizza-topping/pizza-topping.component.ts
+++ src/app/pizza-topping/pizza-topping.component.ts
@@ -32,4 +32,16 @@ export class PizzaToppingComponent implements OnInit {
     this.availablePizzaToppings = this.pizzaSvc.getAvailablePizzaTopping();
   }
 
+  total = 0;
+
+  calculateTotal() {
+    this.total = this.availablePizzaToppings
+      .filter(x => x.checked)
+      .reduce(
+        (acc, x) => acc + x.price 
+        , 0
+      )
+    ;
+  }
+
 }
