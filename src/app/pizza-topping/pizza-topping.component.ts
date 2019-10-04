60254b3f88897db8eded2f9df8c69f277e57ddcc
jpsweeney94 <jpsweeney94@icloud.com>
2019-10-03 19:44:16 -0500

    Using Dependency injected pizza service

--- src/app/pizza-topping/pizza-topping.component.ts
+++ src/app/pizza-topping/pizza-topping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-topping',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingComponent implements OnInit {
 
-  constructor() { }
+  // Magic DI (dependency injection)
+  constructor(private pizzaSvc: PizzaService) { }
+
+  // Longer version of contructor above (above is TypeScript): 
+  // pizzaSvc2: PizzaService;
+  // constructor2Example(pizzaSvc: PizzaService) {
+  //   this.pizzaSvc2 = pizzaSvc;
+  // }
+
+  availablePizzaToppings = [];
 
   ngOnInit() {
+    // Wrong... let angular inject class instances:
+    // const ps = new PizzaService();
+    // const pts = ps.getAvailablePizzaToppings();
+    // console.log(pts);
+
+    this.availablePizzaToppings = this.pizzaSvc.getAvailablePizzaToppings();
+    console.log(this.availablePizzaToppings);
   }
 
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -3,7 +3,21 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class PizzaService {
 
   constructor() { }
+
+  public getAvailablePizzaToppings() {
+    const pizzaToppingsFromWebService = [
+      { name: "Pepperoni", price: 1.5 },
+      { name: "Olives", price: 3.5 }
+    ];
+
+    return pizzaToppingsFromWebService.map(x => ({
+      ... x
+      , checked: true
+    }));
+  }
+
 }
