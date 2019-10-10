910fbf1a2369cfd63d897ecbe928d0ec9fbc2fe4
Jonathan Pabon <35584599+JonnyPabon@users.noreply.github.com>
2019-10-10 18:13:21 -0500

    Changes

--- src/app/pizza-topping/pizza-topping.component.ts
+++ src/app/pizza-topping/pizza-topping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-topping',
@@ -10,6 +11,16 @@ export class PizzaToppingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // new up a pizza service
+
+    const ps = new PizzaService();
+    // call up a pizza toppings
+    const pts = ps.getAvailablePizzaToppings();
+    console.log(pts);
+
+
+
   }
 
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getAvailablePizzaToppings() {
+
+    const pizzaToppingsFromWebService = [
+
+      {name: "Pepperoni", price: 1.5}
+      , {name: "Olives", price: 3.5}
+    ];
+
+    //shaping data
+    return pizzaToppingsFromWebService.map(x => ({
+      ...x
+      , checked: true
+    }));
+  }
 }
