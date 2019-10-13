02552fd7a4b7f380d88d371d442e622efc4b2618
Joe Alexander <cambo7131@gmail.com>
2019-10-13 18:16:05 -0500

    Created slugging percentage component
    Used ng generate component slugging-percentage to generate a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { SluggingPercentageComponent } from './slugging-percentage/slugging-percentage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingComponent
+    PizzaToppingComponent,
+    SluggingPercentageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/slugging-percentage/slugging-percentage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slugging-percentage',
+  templateUrl: './slugging-percentage.component.html',
+  styleUrls: ['./slugging-percentage.component.css']
+})
+export class SluggingPercentageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
