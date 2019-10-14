40089ef7e4fdf748848d423e2097dfe58016b4c4
Kevin Kelm <kkelm@outlook.com>
2019-10-13 22:16:14 -0500

    Finally figured out the passing of coordinates

--- src/app/rotate-triangle/rotate-triangle.component.ts
+++ src/app/rotate-triangle/rotate-triangle.component.ts
@@ -15,12 +15,38 @@ export class RotateTriangleComponent implements OnInit {
 	ngOnInit() {
 
 		this.triangleDimensions = this.triangleService.defaultTriangleDimensions();
-    	console.log(this.triangleDimensions);
+    	//console.log(this.triangleDimensions);
 
 	}
 
 	get coords() {
 		return this.triangleDimensions;
-	  }
+	}
+	get points() {
+		let newPoints: string;
+		
+		newPoints = this.triangleDimensions[0].coordOne.x + ', ';
+		newPoints += this.triangleDimensions[0].coordOne.y + ', ';
+		newPoints += this.triangleDimensions[0].coordTwo.x + ', ';
+		newPoints += this.triangleDimensions[0].coordTwo.y + ', ';
+		newPoints += this.triangleDimensions[0].coordThree.x + ', ';
+		newPoints += this.triangleDimensions[0].coordThree.y;
+
+		console.log(newPoints);
+
+		return newPoints;
+	}
+
+	get pointOneX() {
+		return '10, 0, 100, 100, 0, 100';
+		//return this.triangleDimensions.map(c => ({...c, coordOne: {x: 10} }) );
+	}
+
+	setPointOneX(event) {
+		let xValue: number;
+		xValue = parseFloat(event.target.value);
+		//console.log(xValue);
+		this.triangleDimensions = this.triangleDimensions.map(c => ({...c, coordOne: {x: xValue, y: c.coordOne.y}}) );
+	}
 
 }
