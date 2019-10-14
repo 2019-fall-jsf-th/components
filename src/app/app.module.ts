62246f263675a00ab816e5c80565a20f47472fde
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2019-10-13 19:26:34 -0500

    create greg-card component
    ng generate component greg-card -->
    creates /greg-card directory and
    /greg-card/greg-card.component.* files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { GregCardComponent } from './greg-card/greg-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingComponent
+    PizzaToppingComponent,
+    GregCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/greg-card/greg-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greg-card',
+  templateUrl: './greg-card.component.html',
+  styleUrls: ['./greg-card.component.css']
+})
+export class GregCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
