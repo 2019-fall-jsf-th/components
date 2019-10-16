7b23d6b02a9ab2e5218f46f8b35281662004deb8
andrewprovenzano <54965676+andrewprovenzano@users.noreply.github.com>
2019-10-16 14:36:42 -0500

    Finished with week 6 slack n tell

--- src/app/apweek6/apweek6.component.ts
+++ src/app/apweek6/apweek6.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Apweek6Component implements OnInit {
 
+  numberOne = 10;
+  numberTwo = 5;
+  answer = 15;
+  addNumbers = () => {
+    this.answer = this.numberOne + this.numberTwo;
+  }
   constructor() { }
 
   ngOnInit() {
