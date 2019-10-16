eb18dff53c68dd708c78270392931cbd0f57fefb
Curtis Moore <cwmoore@madisoncollege.edu>
2019-10-16 13:33:27 -0500

    Create component sum-thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { SumThingComponent } from './sum-thing/sum-thing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingComponent
+    PizzaToppingComponent,
+    SumThingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sum-thing/sum-thing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sum-thing',
+  templateUrl: './sum-thing.component.html',
+  styleUrls: ['./sum-thing.component.css']
+})
+export class SumThingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
