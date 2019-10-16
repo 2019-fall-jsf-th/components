c7f50a4f27d0214639df8afcce87dc7b5320890d
Billy Yang <31418367+zobiya@users.noreply.github.com>
2019-10-15 20:16:02 -0500

    generated my component
    will create a wilks calculator for this slack and tell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PizzaToppingComponent } from './pizza-topping/pizza-topping.component';
+import { BillsWilksCalcComponent } from './bills-wilks-calc/bills-wilks-calc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingComponent
+    PizzaToppingComponent,
+    BillsWilksCalcComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bills-wilks-calc/bills-wilks-calc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bills-wilks-calc',
+  templateUrl: './bills-wilks-calc.component.html',
+  styleUrls: ['./bills-wilks-calc.component.css']
+})
+export class BillsWilksCalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
