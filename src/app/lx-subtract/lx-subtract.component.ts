e61d9b73a7d9def8457e2ea75973a0d56b92e92c
gitrlyn <54748346+gitrlyn@users.noreply.github.com>
2019-10-15 22:39:43 -0500

    got subtraction function working

--- src/app/lx-subtract/lx-subtract.component.ts
+++ src/app/lx-subtract/lx-subtract.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lx-subtract.component.css']
 })
 export class LxSubtractComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+  }
+
+  firstNumber = 2;
+  secondNumber = 1;
+  subtractionAnswer = 1;
+
+  subtractNumbers() {
+    this.subtractionAnswer = this.firstNumber - this.secondNumber;
   }
 
 }
