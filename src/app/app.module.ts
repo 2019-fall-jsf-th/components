134bdc5122ae2a29a1786b3dda0073a7bfb5eae1
Joseph Sweeney <jpsweeney94@icloud.com>
2019-10-16 22:16:05 -0500

    Revert "Revert "Added spacing to form components""
    This reverts commit 2785ce50f20328f3e3b6ffc3060c949823821650.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { JpsMathComponent } from './jps-math/jps-math.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { JpsMathComponent } from './jps-math/jps-math.component';
     BrowserAnimationsModule,
     MatCheckboxModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
