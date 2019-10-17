05f13941c2e9dfd36f0d0b5f656279398da842dc
Joseph Sweeney <jpsweeney94@icloud.com>
2019-10-16 21:13:26 -0500

    Generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { JpsMathComponent } from './jps-math/jps-math.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingComponent
+    PizzaToppingComponent,
+    JpsMathComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/jps-math/jps-math.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jps-math',
+  templateUrl: './jps-math.component.html',
+  styleUrls: ['./jps-math.component.css']
+})
+export class JpsMathComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
