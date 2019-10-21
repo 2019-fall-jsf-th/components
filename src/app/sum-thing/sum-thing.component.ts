1f261f5fc99abb40a34d65f711c4692dd2effa84
Curtis Moore <cwmoore@madisoncollege.edu>
2019-10-20 21:43:28 -0500

    Create operations object array and loop to display, access with filter

--- src/app/sum-thing/sum-thing.component.ts
+++ src/app/sum-thing/sum-thing.component.ts
@@ -9,60 +9,37 @@ export class SumThingComponent implements OnInit {
 
     number1 = 0;
     number2 = 0;
-    operator = "sum";
+    operator = '+';
 
-    get sum() {
-        return this.number1 + this.number2;
-    }
-
-    get difference() {
-        return this.number1 - this.number2;
-    }
-
-    get product() {
-        return this.number1 * this.number2;
-    }
-
-    get quotient() {
-        return this.number1 / this.number2;
-    }
+    operations = [
+        {
+            'name': 'sum'
+            , 'symbol': '+'
+            , 'f': (a: number, b: number): number => a + b
+        }
+        , {
+            'name': 'difference'
+            , 'symbol': '-'
+            , 'f': (a: number, b: number): number => a - b
+        }
+        , {
+            'name': 'product'
+            , 'symbol': '*'
+            , 'f': (a: number, b: number): number => a * b
+        }
+        , {
+            'name': 'quotient'
+            , 'symbol': '/'
+            , 'f': (a: number, b: number): number => a / b
+        }
+    ];
 
     calculate() {
-        switch (this.operator) {
-            case "sum":
-                return this.sum;
-
-            case "difference":
-                return this.difference;
-
-            case "product":
-                return this.product;
-
-            case "quotient":
-                return this.quotient;
-
-            default:
-                return this.sum;
-        }
+        return this.operations.filter((op) => op.symbol === this.operator).pop().f(this.number1, this.number2);
     }
 
     symbol() {
-        switch (this.operator) {
-            case "sum":
-                return "+";
-
-            case "difference":
-                return "-";
-
-            case "product":
-                return "*";
-
-            case "quotient":
-                return "/";
-
-            default:
-                return "+";
-        }
+        return this.operator;
     }
 
     constructor() { }
