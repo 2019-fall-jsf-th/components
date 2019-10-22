0aa5776b7e9b2e00accaa762c3ea17b723c6e2fa
corin920 <cheilman920@gmail.com>
2019-10-21 23:08:12 -0500

    Added components in place

--- src/app/random-toppings-caheilman/random-toppings-caheilman.component.ts
+++ src/app/random-toppings-caheilman/random-toppings-caheilman.component.ts
@@ -2,6 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
 @Component({
+    selector: 'slider-overview-example',
+    templateUrl: 'slider-overview-example.html',
+    styleUrls: ['slider-overview-example.css'],
+  })
+  export class SliderOverviewExample {}
+
+  @Component({
+    selector: 'checkbox-overview-example',
+    templateUrl: 'checkbox-overview-example.html',
+    styleUrls: ['checkbox-overview-example.css'],
+  })
+  export class CheckboxOverviewExample {}
+  
+  @Component({
+    selector: 'slide-toggle-overview-example',
+    templateUrl: 'slide-toggle-overview-example.html',
+    styleUrls: ['slide-toggle-overview-example.css'],
+  })
+  export class SlideToggleOverviewExample {}
+
+  @Component({
   selector: 'app-random-toppings-caheilman',
   templateUrl: './random-toppings-caheilman.component.html',
   styleUrls: ['./random-toppings-caheilman.component.css']
@@ -9,4 +30,3 @@ import { PizzaService } from '../pizza.service';
 export class RandomToppingsCaheilmanComponent {
     
 }
-console.log('This worked!');
--- src/app/random-toppings-caheilman/random-toppings-caheilman.spec.ts
+++ src/app/random-toppings-caheilman/random-toppings-caheilman.spec.ts
@@ -1,3 +1,6 @@
+import { SliderOverviewExample } from './random-toppings-caheilman.component';
+import { CheckboxOverviewExample } from './random-toppings-caheilman.component';
+import { SlideToggleOverviewExample } from './random-toppings-caheilman.component';
 import { RandomToppingsCaheilmanComponent } from './random-toppings-caheilman.component';
 
 describe('RandomToppingsCaheilman', () => {
@@ -7,3 +10,4 @@ describe('RandomToppingsCaheilman', () => {
     expect(component).toBeTruthy();
   });
 });
+
