c572478bee7366d33e2de7f11feeebaedc40de4a
corin920 <cheilman920@gmail.com>
2019-12-20 18:32:24 -0600

    inputs works, button works
    Now I just need to have the button produce output to the html...

--- src/app/caheilman-random-number/caheilman-random-number.component.ts
+++ src/app/caheilman-random-number/caheilman-random-number.component.ts
@@ -8,23 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class CaheilmanRandomNumberComponent implements OnInit {
 
   ngOnInit() { 
-    this.randomNumber = 0;
   }
 
   randomMin=0
   randomMax=10
-  randomNumber=0
+  // randomNumber=0
 
   randomize() {
-    // console.log("this works")
+    console.log("this works")
     if ( this.randomMin > this.randomMax) {
       var errorNumber = "Minimum is greater than Maximum. Please try again."
+      console.log(errorNumber);
     } else {
      var randomNumber = Math.floor((Math.random() * this.randomMax) + this.randomMin);
       console.log(randomNumber);
-      return this.randomNumber
     }
-    
   }
+  
+  // // get randomNumber
 
 }
